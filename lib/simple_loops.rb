c51035e370c1c1c6d4a800130bcf882e657c23ca
John Easter <john@owltestprep.com>
2020-07-08 01:29:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
-# Write your methods here
+def loop_message_five_times(message)
+  message = "Hello World"
+  counter = 0
+    while counter <= 5
+    puts message
+    counter += 1
+  end
+end
+
+def loop_message_n_times
+  
+end
+
+def output_array
+  
+end
+
+def return_string_array
+  
+end
