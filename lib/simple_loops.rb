049496297e1195cc0ab95ee21f9220f6555fdb4b
John Easter <john@owltestprep.com>
2020-07-08 01:40:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,4 +6,10 @@ def loop_message_five_times(message = "Hello World")
   end
 end
 
-def loop_message_n_times(message = "Hello Wolrd")
+def loop_message_n_times(message = "Hello Wolrd", n = 10)
+  counter = 10
+  while counter <= n do
+    puts message
+    counter += 1
+  end
+end
