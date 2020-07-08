eb1c96eb69939c7db3021274577f3f0192675a74
John Easter <john@owltestprep.com>
2020-07-08 01:45:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,4 +14,10 @@ def loop_message_n_times(message = "Hello Wolrd", n = 10)
   end
 end
 
-output_array(array = ["All", "great", "achievments", "require", "time", "-Maya Angelou"])
+def output_array(array = ["All", "great", "achievments", "require", "time", "-Maya Angelou"])
+  counter = 0
+  While array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
